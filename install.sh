7a97fba912784860b201792d7402159fde42b5fa
Denis Kuzmin <nirdest@gmail.com>
2020-11-01 01:13:39 +0600

    Fix second problem

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 sudo mkdir /usr/sbin/winbox
 cp ./winbox.desktop ~/Desktop
+sudo chmod ugo+x ~/Desktop/winbox.desktop
+sudo cp ./icon.png /usr/sbin/winbox/icon.png
 sudo cp ./winbox.desktop /usr/share/applications/winbox.desktop
 cd /usr/sbin/winbox
 sudo wget https://mt.lv/winbox64
