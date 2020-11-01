a8ab5a84c86518f441e213cb721991297a28576c
Denis Kuzmin <nirdest@gmail.com>
2020-11-01 11:41:31 +0600

    Last Fix

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 sudo mkdir /usr/sbin/winbox
-cp ./winbox.desktop ~/Desktop
-sudo chmod ugo+x ~/Desktop/winbox.desktop
 sudo cp ./icon.png /usr/sbin/winbox/icon.png
+cp ./winbox.desktop ~/Desktop
 sudo cp ./winbox.desktop /usr/share/applications/winbox.desktop
 cd /usr/sbin/winbox
 sudo wget https://mt.lv/winbox64
-alias winbox='wine ./winbox64 > /dev/null 2>&1 & '
+echo 'alias winbox="wine /usr/sbin/winbox/winbox64 > /dev/null 2>&1 &"' >> ~/.bashrc
